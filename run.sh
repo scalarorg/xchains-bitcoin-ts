ef9ee95f35a02a47de834c18aa43cb167ae3b31f
Vu Viet Tai <vuviettai@gmail.com>
2024-09-17 08:12:37 +0700

    Update script for both linux & Macos

--- run.sh
+++ run.sh
@@ -5,11 +5,17 @@ BITCOINDIR=/bitcoin
 copy_local_env() {
     NETWORK=$1
     cp ${DIR}/../.testnets/bitcoin/.env ${DIR}/.env
-    # On macos use -i '' instead of -i
-    sed -i '' 's|BITCOIN_HOST=.*|BITCOIN_HOST=localhost|g' ".env"
-    sed -i '' 's|MEMPOOL_URL=.*|MEMPOOL_URL=http://localhost:8999/api|g' ".env"
     SERVICE_CONTRACT=$(jq -r '.mintContract' ${DIR}/../.testnets/chains/${NETWORK}/addresses.json)
-    sed -i '' "s|DEST_SMART_CONTRACT_ADDRESS=.*|DEST_SMART_CONTRACT_ADDRESS=${SERVICE_CONTRACT}|g" ".env"
+    PLATFORM=$(uname)
+    if [ "$PLATFORM" == "Linux" ]; then
+        sed -i 's|BITCOIN_HOST=.*|BITCOIN_HOST=localhost|g' ".env"
+        sed -i 's|MEMPOOL_URL=.*|MEMPOOL_URL=http://localhost:8999/api|g' ".env"
+        sed -i "s|DEST_SMART_CONTRACT_ADDRESS=.*|DEST_SMART_CONTRACT_ADDRESS=${SERVICE_CONTRACT}|g" ".env"
+    elif [ "$PLATFORM" == "Darwin" ]; then
+        sed -i '' 's|BITCOIN_HOST=.*|BITCOIN_HOST=localhost|g' ".env"
+        sed -i '' 's|MEMPOOL_URL=.*|MEMPOOL_URL=http://localhost:8999/api|g' ".env"
+        sed -i '' "s|DEST_SMART_CONTRACT_ADDRESS=.*|DEST_SMART_CONTRACT_ADDRESS=${SERVICE_CONTRACT}|g" ".env"    
+    fi
 }
 
 copy_env() {
